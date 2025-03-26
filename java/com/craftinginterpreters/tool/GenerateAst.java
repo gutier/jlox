7a8e55bdfe9500024fcb057470ee076dcc40dbd8
gutier <gutier@protonmail.com>
2025-03-26 19:31:10 +0100

    implements print and expr statements

--- java/com/craftinginterpreters/tool/GenerateAst.java
+++ java/com/craftinginterpreters/tool/GenerateAst.java
@@ -11,12 +11,18 @@ public class GenerateAst {
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(64);
         }
-        String ouputDir = args[0];
-        defineAst(ouputDir, "Expr", Arrays.asList(
+        String outputDir = args[0];
+
+        defineAst(outputDir, "Expr", Arrays.asList(
             "Binary   : Expr left, Token operator, Expr right",
             "Grouping : Expr expression",
             "Literal  : Object value",
             "Unary    : Token operator, Expr right"
+        ));
+
+        defineAst(outputDir, "Stmt", Arrays.asList(
+        "Expression : Expr expression",
+        "Print      : Expr expression"
         ));
     }
     private static void defineAst(
