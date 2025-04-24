a4afc778013c52599f8aad0f253211bccbe9d615
gutier <gutier@protonmail.com>
2025-04-24 11:24:44 +0200

    adds expr stmts

--- c/chunk.h
+++ c/chunk.h
@@ -10,6 +10,7 @@ typedef enum {
   OP_TRUE,
   OP_FALSE,
   OP_EQUAL,
+  OP_POP,
   OP_GREATER,
   OP_LESS,
   OP_ADD,
--- c/compiler.c
+++ c/compiler.c
@@ -271,6 +271,12 @@ static void expression() {
   parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+  expression();
+  consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+  emitByte(OP_POP);
+}
+
 static void printStatement() {
   expression();
   consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -284,6 +290,9 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else {
+    expressionStatement();
+
   }
 }
 
